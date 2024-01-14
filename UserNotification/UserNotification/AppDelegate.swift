46aa3a8158fefd0f4f409f62a57aaaff6e65c27a
Divya-Maloo1207 <maloodivya25@gmail.com>
2024-01-14 17:50:47 +0530

    registeredApp for Remote Notifications

--- UserNotification/UserNotification/AppDelegate.swift
+++ UserNotification/UserNotification/AppDelegate.swift
@@ -5,7 +5,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.shared.registerForRemoteNotifications()
         return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // Call server and pass device token
     }
 
     // MARK: UISceneSession Lifecycle
